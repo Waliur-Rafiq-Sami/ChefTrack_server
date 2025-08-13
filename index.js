fbf696fc42a83e4168656884281ff981c6d0223c
SAM's pc <Waliur Rafiq Sami>
2025-08-13 19:05:25 +0600

    update add food api

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ytzwbuc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -23,6 +28,60 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // Foods
+    const FoodsCollection = client
+      .db("chefTrack")
+      .collection("FoodsCollection");
+
+    // users
+    const userInformationCollection = client
+      .db("chefTrack")
+      .collection("userInformation");
+
+    //userInformation
+    app.get("/profile/:email", async (req, res) => {
+      const userEmail = req.params.email;
+      // console.log(userEmail);
+      const query = { email: userEmail };
+      const result = await userInformationCollection.findOne(query);
+      res.send(result);
+    });
+    app.put("/profile", async (req, res) => {
+      const addUser = req.body;
+      const userEmail = addUser.email;
+      // console.log("Received email:", userEmail);
+      // console.log("Received:", addUser);
+      const filter = { email: userEmail };
+      const updateDoc = {
+        $set: {
+          displayName: addUser.displayName,
+          phone: addUser.phone,
+          photoURL: addUser.photoURL,
+          address: addUser.address,
+        },
+      };
+      const result = await userInformationCollection.updateOne(
+        filter,
+        updateDoc,
+        { upsert: true }
+      );
+      res.send(result);
+    });
+
+    //foods
+
+    // AddFood
+    app.post("/addFoods", async (req, res) => {
+      const newFoodItem = req.body;
+      // console.log(newFoodItem);
+      const result = await FoodsCollection.insertOne(newFoodItem);
+      res.send(result);
+    });
+
+    //allFoods
+    app.get("/allFoods");
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -30,7 +89,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
